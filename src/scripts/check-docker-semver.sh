e347d27db85d356e385062bdb1cae30be9231fa7
Cwagne17 <cwagne17@students.towson.edu>
2022-03-31 12:22:55 -0400

    added status and output checks

--- src/scripts/check-docker-semver.sh
+++ src/scripts/check-docker-semver.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 set -o nounset # exit script if unset variable is used
-set -o errexit # exit script if statement returns a non-true return value
-set -o pipefail # use erorr status of first failure, rather than last item in pipeline
 
 check_version() {
     if curl --silent -f -lSL "https://index.docker.io/v1/repositories/cyber4all/${DOCKER_REPOSITORY}/tags/${DOCKER_SEMVER}" > /dev/null; then
